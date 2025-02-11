968f1f84676ee0d34bd2ece37aec006b39b7a007
Diogo <diogo.miguel.silva.gaspar@gmail.com>
2025-02-11 23:52:25 +0000

    Added: UI folder, Constants

---
+++ src/constants.py
@@ -0,0 +1,17 @@
+# Screen
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+
+#
+FPS = 60
+
+# Colour Palette
+OFF_WHITE = (220, 220, 220)
+GREY = (169, 169, 169)
+DARK_GREY = (128, 128, 128)
+ORANGE = (255, 99, 71)
+RED = (178, 34, 34)
+
+# buttons text
+# (35, 35, 35)
+# (80, 80, 80)
